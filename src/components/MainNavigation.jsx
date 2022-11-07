8bc1206655107beef69cd24acc72411acf97b9f7
Santiago Caci <santiago.caci@gmail.com>
2022-11-07 17:00:18 -0300

    Add ErrorPage

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,6 +1,6 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,39 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-
-import BlogPost from '../components/BlogPost';
-import { getPost } from '../util/api';
+import { redirect, useLoaderData } from "react-router-dom";
+import BlogPost from "../components/BlogPost";
+import { getPost } from "../util/api";
 
 function PostDetailPage() {
-  const [error, setError] = useState();
-  const [post, setPost] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const params = useParams();
-  const { id } = params;
-
-  useEffect(() => {
-    async function loadPost() {
-      setIsLoading(true);
-      try {
-        const post = await getPost(id);
-        setPost(post);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
-    }
-
-    loadPost();
-  }, [id]);
-
+  const postData = useLoaderData();
   return (
     <>
-      {isLoading && <p>Loading post...</p>}
-      {error && <p>{error.message}</p>}
-      {!error && post && <BlogPost title={post.title} text={post.body} />}
+      <BlogPost title={postData.title} text={postData.body} />
     </>
   );
+}
+
+export async function loader({ params }) {
+  const id = params.id;
+  const post = await getPost(id);
+  return post;
 }
 
 export default PostDetailPage;
